694a5b90fb4f00f6cd35eb511597734c969e1bf8
Felipe Salles <prof.felipe.salles@gmail.com>
2022-04-12 00:36:38 -0300

    Organiza repositorio para monorepo

---
+++ estudo-kafka-consumer/src/main/java/felipesalles/estudokafkaconsumer/EstudoKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package felipesalles.estudokafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ estudo-kafka-consumer/src/test/java/felipesalles/estudokafkaconsumer/EstudoKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package felipesalles.estudokafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
